9f7043ad24331a64245f905f550f92cd0ccd4942
Rayshawn Summers <Rayshawn.summers@sciacademy.org>
2023-09-19 20:44:40 +0000

    <made right cannon slower and left cannon faster and kept top the cannon same speed >

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -63,8 +63,8 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("left", 600,10000,)
-    createCannon("right",600,10000,)
+    createCannon("left", 600,2000,)
+    createCannon("right",600,5000,)
     createCannon("top",580,500)
 
 

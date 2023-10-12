71c8471c6620df513ed5a7535353b388de0d61e5
Rayshawn Summers <Rayshawn.summers@sciacademy.org>
2023-10-12 20:43:41 +0000

    add bouncing box

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -63,7 +63,7 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("left", 600,4000,)
+    createCannon("left", 600,3000,)
     createCannon("right",600,3000,)
     createCannon("top",580,1000)
 
